0bc104c3889a9c5a69512e6c280ddc0743f7a261
Marine <marine.iguna@gmail.com>
2023-05-09 13:17:04 +0200

    feat (pages) : add necessary folder and pages and set up Sass environment
    Add home, about, 404 and location pages and remove files such as reportWebVitals and setupTests. Set up Sass environment by renaming index.css to index.scss. Replace favicon.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import logo  from './assets/LOGO.svg'
+import Header from './layout/Header';
+import Footer from './layout/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import Location from './pages/Locations';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header logo={logo}/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/locations/:id" element={<Location/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
+  
   );
 }
 
