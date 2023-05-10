24bc1234b42654b7029ac15c3d40a9a7d473c922
Marine <marine.iguna@gmail.com>
2023-05-10 11:34:42 +0200

    feat(routing) : Add routing for pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import logo  from './assets/LOGO.svg'
-import Header from './layout/Header';
-import Footer from './layout/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import NotFound from './pages/NotFound';
-import Location from './pages/Locations';
+import { BrowserRouter as Router } from "react-router-dom";
+
+import logo from "./assets/LOGO.svg";
+import Header from "./layout/Header";
+import AppRouter from "./components/AppRouter";
+import Footer from "./layout/Footer";
 
 function App() {
   return (
     <Router>
-      <Header logo={logo}/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/locations/:id" element={<Location/>}/>
-        <Route path="*" element={<NotFound/>}/>
-      </Routes>
-      <Footer/>
+      <Header logo={logo} />
+      <main>
+        <AppRouter />
+      </main>
+      <Footer />
     </Router>
-  
   );
 }
 
---
+++ src/components/AppRouter.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Location from "../pages/Locations";
+import NotFound from "../pages/NotFound";
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/locations/:id" element={<Location />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default AppRouter;
