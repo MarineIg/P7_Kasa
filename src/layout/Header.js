d468b571e0f58adc5e295397b4e53186dbe7422f
Marine <marine.iguna@gmail.com>
2023-05-15 19:00:55 +0200

    refactor (header) : Removed navbar into header, replaced Navbar component with a nav element within Header component for improved structure and organization.

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,12 +1,23 @@
-import NavBar from "../components/NavBar";
+import { NavLink } from "react-router-dom";
 import Logo from "../components/Logo";
-import logoHeader from "../assets/logoHeader.svg"
+import logoHeader from "../assets/logoHeader.svg";
 
 function Header() {
   return (
     <header className="header">
-      <Logo src={logoHeader} />
-      <NavBar />
+      <div className="header__content">
+        <Logo src={logoHeader} />
+        <nav className="header__nav">
+          <ul>
+            <li>
+              <NavLink to="/" className={(nav) => (nav.isActive ? "header__nav--active" : "")}>Accueil</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" className={(nav) => (nav.isActive ? "header__nav--active" : "")}>A Propos</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
