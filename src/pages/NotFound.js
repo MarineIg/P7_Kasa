5df2a78199f13025f4e3a0197025bb2b6221b327
Marine <marine.iguna@gmail.com>
2023-05-16 19:25:11 +0200

    feat(notFound) : Add notFound page and style

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
-    return (
-        <div className='notFound'>
-            <h1 className='notFound__title'>404</h1>
-            <p className='notFound__text'>Oups! La page que vous demandez n'existe pas.</p>
-            <Link to="/">
-                <p className='notFound__link'>Retourner sur la page d'accueil</p>
-            </Link>
-        </div>
-    );
+  return (
+    <div className="notFound">
+      <h1 className="notFound__title">404</h1>
+      <h2 className="notFound__subtitle">
+        Oups! La page que vous demandez n'existe pas.
+      </h2>
+      <Link to="/">
+        <p className="notFound__link">Retourner sur la page d'accueil</p>
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
