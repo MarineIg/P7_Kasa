157479885bac77987420047ff80fe13e53e1ac3b
Marine <marine.iguna@gmail.com>
2023-05-30 18:10:08 +0200

    update(collapse): Add PropTypes and defaultProps

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,25 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 import arrowIcon from "../assets/dropdown-arrow.svg";
 import { useState } from "react";
 
 const Collapse = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const {
-    borderRadius = "5px",
-    width = "100%",
-    fontSize = "24px",
-    height = "47px",
-  } = props;
-
   const collapseStyle = {
-    borderRadius,
-    width,
-    fontSize,
+    borderRadius: props.borderRadius,
+    width: props.width,
+    fontSize: props.fontSize,
   };
 
   const heightStyle = {
-    height,
+    height: props.height,
   };
 
   return (
@@ -34,11 +28,30 @@ const Collapse = (props) => {
           <img src={arrowIcon} alt="Icône de flèche" />
         </span>
       </button>
-      <div className={isOpen ? "collapse__text collapse__text--open" : "collapse__text"}>
+      <div
+        className={
+          isOpen ? "collapse__text collapse__text--open" : "collapse__text"
+        }
+      >
         {props.children}
       </div>
     </div>
   );
+};
+Collapse.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  borderRadius: PropTypes.string,
+  width: PropTypes.string,
+  fontSize: PropTypes.string,
+  height: PropTypes.string,
+};
+
+Collapse.defaultProps = {
+  borderRadius: "5px",
+  width: "100%",
+  fontSize: "24px",
+  height: "47px",
 };
 
 export default Collapse;
