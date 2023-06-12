6ec5899355b5fbbacdc9dfb63d5a3b730d5addaa
Marine <marine.iguna@gmail.com>
2023-06-12 13:15:35 +0200

    refactor(StarRating) : - Update star icons to use SVG format. - Add JSDoc comments to document the component and its props. This commit refactors the StarRating component by replacing the star icons with SVG format for better scalability and customization. Additionally, JSDoc comments have been added to improve code documentation and readability.

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,6 +1,12 @@
-import React from "react";
-import starPink from "../assets/star_pink.svg";
-import starGrey from "../assets/star_grey.svg";
+import PropTypes from "prop-types";
+import Star from "../assets/star.svg";
+
+/**
+ * This component displays a star rating.
+ * @param {object} props - The props for the component.
+ * @param {number} props.rating - This rating to display.
+ * @returns {JSX.Element} The StarRating component.
+ */
 
 const StarRating = ({ rating }) => {
   const score = [1, 2, 3, 4, 5];
@@ -8,15 +14,23 @@ const StarRating = ({ rating }) => {
   return (
     <div className="starRating">
       {score.map((rateElem, index) => (
-        <img
-          src={rating >= rateElem ? starPink : starGrey}
-          alt="étoile"
-          key={index}
-          className="star"
-        />
+        <svg
+          key={`star ${index}`}
+          width="25"
+          height="24"
+          className={`star
+            ${rating >= rateElem ? "star--filled" : "star--empty"}`}
+        >
+          <use xlinkHref={`${Star}#Icon-Star`} />
+        </svg>
       ))}
     </div>
   );
+};
+
+// Define propTypes for the component
+StarRating.propTypes = {
+  rating: PropTypes.number.isRequired,
 };
 
 export default StarRating;
